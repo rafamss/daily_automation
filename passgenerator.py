5c96194eabe4bc2cb8e443167a86387c105d2eac
Rafael Martins <rafamss@gmail.com>
2018-06-16 17:17:42 -0300

    change the print output

--- passgenerator.py
+++ passgenerator.py
@@ -9,4 +9,3 @@ allchar = string.ascii_letters + string.punctuation + string.digits
 password = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
 
 print ("Your password is marked in red " + '\x1b[1;37;41m' + password + '\x1b[0m' + " and was generated with"  + '\x1b[1;32;40m', len(password), '\x1b[0m'+"characters of length.")
-#print('\x1b[1;37;41m' + password + '\x1b[0m')
